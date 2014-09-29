1d520a9c9a7c312a52b88e1444349cc941e95dae
akarsha <akarsh.agarwala@imaginea.com>
2014-09-29 15:00:04 +0530

    Design change in js, encapsulated all the calculator functionality in a single object.

--- Calculator/src/calculator.js
+++ Calculator/src/calculator.js
@@ -1,48 +1,54 @@
-function dataObject(){
-	this.latestOperator = null;
-	this.prevOperand = "";
-	this.curOperand = "";
-}
-//TODO: is there any way to encapsulate data and its related functions better
-//Alternatively we can encapsulate all the functions and data in constructor function and can have global object of that function,
-//Need to confirm the approach
-var data = new dataObject();
-function handleClickInput(button, textFieldId){
-	var text = document.getElementById(textFieldId);
-	var btValue = button.value;
-	handleCalculator(btValue,text);
-}
 
-function handleCalculator(btValue,text){
-	if(btValue=="+" || btValue=="-" || btValue=="*" || btValue=="/"){
-		if(data.latestOperator!=null && data.curOperand!="" && data.prevOperand!=""){
-			data.prevOperand = operate();
-			data.latestOperator = btValue;
-			data.curOperand = "";
-			text.value = data.prevOperand;
-		} else if(data.curOperand!=""){
-			data.latestOperator = btValue;
-			data.prevOperand = data.curOperand;
-			data.curOperand = "";
-		} 
+// is there any way to encapsulate data and its related functions better
+//Alternatively we can encapsulate all the functions and data in constructor function and can have global object of that function,
+
+function Calculator(){
+	this.data = new dataObject();
+	function dataObject(){
+		this.latestOperator = null;
+		this.prevOperand = "";
+		this.curOperand = "";
 	}
-	if(btValue=="C"){
-		data.curOperand = "";
-		text.value = "";
+	this.handleClickInput = function handleClickInput(button, textFieldId){
+		var text = document.getElementById(textFieldId);
+		var btValue = button.value;
+		handleCalculator(btValue,text,this);
 	}
-	if((btValue>="0" && btValue<="9") || btValue=="."){
-		data.curOperand = data.curOperand + btValue;
-		text.value = data.curOperand;
-	}
-	if(btValue=="="){
-		if(data.latestOperator!=null && data.prevOperand!="" && data.curOperand!=""){
-			data.curOperand = operate();
-			data.prevOperand = "";
-			data.latestOperator = null;
-			text.value = data.curOperand;
+	function handleCalculator(btValue,text,calculator){
+		if(btValue=="+" || btValue=="-" || btValue=="*" || btValue=="/"){
+			if(calculator.data.latestOperator!=null && calculator.data.curOperand!="" && calculator.data.prevOperand!=""){
+				calculator.data.prevOperand = operate(calculator);
+				calculator.data.latestOperator = btValue;
+				calculator.data.curOperand = "";
+				text.value = calculator.data.prevOperand;
+			} else if(calculator.data.curOperand!=""){
+				calculator.data.latestOperator = btValue;
+				calculator.data.prevOperand = calculator.data.curOperand;
+				calculator.data.curOperand = "";
+			} 
+		}
+		if(btValue=="C"){
+			calculator.data.curOperand = "";
+			text.value = "";
+		}
+		if((btValue>="0" && btValue<="9") || btValue=="."){
+			calculator.data.curOperand = calculator.data.curOperand + btValue;
+			text.value = calculator.data.curOperand;
+		}
+		if(btValue=="="){
+			if(calculator.data.latestOperator!=null && calculator.data.prevOperand!="" && calculator.data.curOperand!=""){
+				calculator.data.curOperand = operate(calculator);
+				calculator.data.prevOperand = "";
+				calculator.data.latestOperator = null;
+				text.value = calculator.data.curOperand;
+			}
+		}
+		function operate(calculator){
+			return eval(calculator.data.prevOperand+calculator.data.latestOperator+calculator.data.curOperand);
 		}
 	}
-	function operate(){
-		return eval(data.prevOperand+data.latestOperator+data.curOperand);
-	}
+	
 }
+var CALCULATOR = new Calculator();
+
+
